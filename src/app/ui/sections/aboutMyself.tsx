553809be2b40ad76deb532174c887897e571bde9
“rich” <adukyerer@gmail.com>
2024-05-30 00:08:03 +0000

    Fix eslint errors

--- src/app/ui/sections/aboutMyself.tsx
+++ src/app/ui/sections/aboutMyself.tsx
@@ -9,19 +9,19 @@ const AboutMyself: React.FC = () => {
             <section className="mb-8">
                 <Heading type='h2' className="text-3xl font-bold mb-4">About Myself</Heading >
                 <p className="mb-4 text-gray-600">
-                    Hey there, I'm a software developer passionate about crafting digital solutions. Whether it's building a website, an app, or implementing new features, I'm here to help. Take a look at my portfolio and experience, and if you have a project in mind, let's chat!
+                    Hey there, I&apos;m a software developer passionate about crafting digital solutions. Whether it&apos;s building a website, an app, or implementing new features, I&apos;m here to help. Take a look at my portfolio and experience, and if you have a project in mind, let&apos;s chat!
                 </p>
             </section>
 
             <section className="mb-8">
                 <Heading type='h2' className="text-3xl font-bold mb-4">Experience</Heading>
                 <p className="text-gray-600 mb-4">
-                    With several years of experience in software development, I've worked on a variety of projects spanning web development, mobile apps, and backend systems. From startups to established companies, I've collaborated with diverse teams to deliver innovative solutions.
+                    With several years of experience in software development, I&apos;ve worked on a variety of projects spanning web development, mobile apps, and backend systems. From startups to established companies, I&apos;ve collaborated with diverse teams to deliver innovative solutions.
                 </p>
             </section>
 
             <section>
-                <Heading type='h2' className="text-3xl font-bold mb-4">Let's Connect</Heading>
+                <Heading type='h2' className="text-3xl font-bold mb-4">Let&apos;s Connect</Heading>
                 <SocialIcons />
                 <Button className='my-8'>Download my resume</Button>
             </section>
