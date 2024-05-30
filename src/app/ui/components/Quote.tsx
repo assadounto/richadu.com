845240e9ab2c69f24c81dc0c5a95ce9b552f8216
“rich” <adukyerer@gmail.com>
2024-05-30 00:27:12 +0000

    Fix escape chars

--- src/app/ui/components/Quote.tsx
+++ src/app/ui/components/Quote.tsx
@@ -9,7 +9,7 @@ interface QuoteProps {
 const Quote: React.FC<QuoteProps> = ({ text, author }) => {
     return (
         <div className="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg shadow-md my-4">
-            <p className="italic text-lg text-gray-700 dark:text-gray-300">"{text}"</p>
+            <p className="italic text-lg text-gray-700 dark:text-gray-300">&quot;{text}&quot;</p>
             <p className="text-right mt-2 text-gray-600 dark:text-gray-400">- {author}</p>
         </div>
     );
