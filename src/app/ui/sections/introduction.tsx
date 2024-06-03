5681a554da0404fec05e69c9aad3f12b3a341baa
“rich” <adukyerer@gmail.com>
2024-06-03 10:51:25 +0000

    Update resume link

--- src/app/ui/sections/introduction.tsx
+++ src/app/ui/sections/introduction.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa';
 import Button from "../components/Button";
 import Heading from "../components/Heading";
+import SocialIcons from "../components/Scials";
 
 const Introductions: React.FC = () => {
     return (
@@ -12,7 +13,7 @@ const Introductions: React.FC = () => {
             <Heading type="h1" className="mt-1 text-3xl md:text-5xl 2xl:text-6xl" data-fade="2">
                 You can call me <span className="transition-colors bg-gradient-to-tr from-primary-300/40 via-primary-300/40 to-primary-400/40 dark:from-primary-300 dark:text-link dark:bg-clip-text dark:text-primay">Richmond</span>
             </Heading>
-            <p className="mt-2 max-w-4xl eading-relaxed text-gray-400 dark:text-gray-200 md:mt-3 text-sm md:text-base 2xl:text-lg" data-fade="2">
+            <p className="mt-2 max-w-4xl eading-relaxed text-gray-700 font800 dark:text-gray-200 md:mt-3 text-sm md:text-base 2xl:text-lg" data-fade="2">
                 Software and Electrical Engineer.
             </p>
             <p className="mt-4 max-w-4xl text-gray-600 dark:text-gray-200 md:mt-6 md:text-lg 2xl:text-xl" data-fade="3">
@@ -23,24 +24,13 @@ const Introductions: React.FC = () => {
             </p>
             <div data-fade="5" className="mt-8 flex flex-wrap gap-4 md:!text-lg">
                 <div className="group relative">
-                    <Button>View Projects</Button>
+                    <Button href="/projects">View Projects</Button>
                 </div>
-                
-                <Button>About me</Button>
+
+                <Button href="/about">About me</Button>
             </div>
-            <div data-fade="6" className="mt-4 flex flex-wrap gap-4 gap-y-2 md:mt-8">
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/your-github" className="inline-flex items-center gap-1 text-sm font-medium md:text-base text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white focus:outline-none focus-visible:ring focus-visible:ring-primary-300 transition-colors cursor-newtab">
-                    <FaGithub className="dark:text-white text-blue-600" size={24} />
-                    <span className="text-blue-600 dark:text-white">Github</span>
-                </a>
-                <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/your-twitter" className="inline-flex items-center gap-1 text-sm font-medium md:text-base group text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white focus:outline-none focus-visible:ring focus-visible:ring-primary-300 transition-colors cursor-newtab">
-                    <FaTwitter size={24} className="dark:text-white transition-colors group-hover:text-[#1da1f2]" />
-                    <span className="text-blue-600 dark:text-white">@your-twitter</span>
-                </a>
-                <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/your-linkedin" className="inline-flex items-center gap-1 text-sm font-medium md:text-base text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white focus:outline-none focus-visible:ring focus-visible:ring-primary-300 transition-colors cursor-newtab">
-                    <FaLinkedin className="dark:text-white text-blue-600" size={24} />
-                    <span className="text-blue-600 dark:text-white">LinkedIn</span>
-                </a>
+            <div data-fade="6" className="mt-6 flex flex-wrap gap-4 gap-y-2 md:mt-8">
+                <SocialIcons />
             </div>
         </article>
     );
